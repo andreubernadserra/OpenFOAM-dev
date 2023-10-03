b7ae71876938b44502d8e1a6b07d624f7b6ece35
Andreu BERNAD <andreubernadserra@outlook.com>
2023-10-03 22:04:21 +0300

    Test case for the findSphere method

--- test/fvMeshTools/Test-findSphere/Test-findSphere.C
+++ test/fvMeshTools/Test-findSphere/Test-findSphere.C
@@ -13,21 +13,12 @@ Description
 #include "fvMeshTools.H"
 #include "indexedOctree.H"
 #include "treeDataCell.H"
+#include "treeDataFace.H"
+#include "dimensionedScalar.H"
 
 using namespace Foam;
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
-
-labelList testFindSphere
-(
-    const indexedOctree<treeDataCell>& cellTree
-)
-{
-    const scalar radiusSqr = 9e-4;
-    const point centre = {5e-2, 5e-2, 5e-3};
-
-    return cellTree.findSphere(centre, radiusSqr);
-}
 
 int main(int argc, char *argv[])
 {
@@ -37,7 +28,79 @@ int main(int argc, char *argv[])
     #include "createTimeNoFunctionObjects.H"
     #include "createNamedMesh.H"
 
-    const labelList cellsWithinSphere = testFindSphere(mesh.cellTree());
+    const scalar radius = 2.5e-2;
+    const point centre = {5e-2, 5e-2, 5e-3};
+    Info<< "Sphere with radius " << radius << " and centre " << centre << "\n";
+
+
+    Info<< "Testing the treeDataCell class\n";
+    volScalarField overlapCells
+    (
+        IOobject
+        (
+            "overlapCells",
+            runTime.name(),
+            mesh,
+            IOobject::NO_READ,
+            IOobject::NO_WRITE
+        ),
+        mesh,
+        dimensionedScalar("overlaps", dimless, 0)
+    );
+
+    const indexedOctree<treeDataCell>& cellTree = mesh.cellTree();
+    const labelList cells = cellTree.findSphere(centre, sqr(radius));
+
+    forAll(cells, i)
+    {
+        overlapCells[cells[i]] ++;
+    }
+
+    
+    Info<< "Testing the treeDataFace class\n";
+    surfaceScalarField overlapFaces
+    (
+        IOobject
+        (
+            "overlapFaces",
+            runTime.name(),
+            mesh,
+            IOobject::NO_READ,
+            IOobject::NO_WRITE
+        ),
+        mesh,
+        dimensionedScalar("overlaps", dimless, 0)
+    );
+
+    labelList internalFaces(mesh.nInternalFaces());
+    forAll(internalFaces, facei)
+    {
+        internalFaces[facei] = facei;
+    }
+
+    indexedOctree<treeDataFace> faceTree(
+        treeDataFace(
+            false, 
+            mesh,
+            internalFaces
+        ),
+        treeBoundBox(mesh.bounds()).extend(1e-4),
+        8,
+        0.5,
+        1.0
+    );
+
+    const labelList faces = faceTree.findSphere(centre, sqr(radius));
+
+    forAll(faces, i)
+    {
+        overlapFaces[faces[i]] ++;
+    }
+
+
+    runTime ++;
+    overlapCells.write();
+    overlapFaces.write();
 
     Info<< "End\n" << endl;
 
